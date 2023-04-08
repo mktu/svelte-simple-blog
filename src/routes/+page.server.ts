1bb145b86f0cbc08671c7aed29c89bf9c5b380b3
mktu <mushi9ui@gmail.com>
2023-04-08 21:47:53 +0900

    Adapter-static not applying CSS on index page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,3 +1,5 @@
+// https://github.com/sveltejs/kit/issues/9404
+export const trailingSlash = 'always';
 import { getAuther } from '$lib/newt/client';
 import type { PageServerLoad } from './$types';
 

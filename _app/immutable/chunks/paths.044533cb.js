d01d33bc7fed38056525dc46cfd35f5edad62c33
mktu <mktu@users.noreply.github.com>
2023-06-17 14:57:29 +0000

    deploy: 75a50d0bd26b2492e741e04b3e896d7fc286b49f

--- _app/immutable/chunks/paths.044533cb.js
+++
@@ -1,1 +0,0 @@
-var s;const l=((s=globalThis.__sveltekit_lskyv2)==null?void 0:s.base)??"/svelte-simple-blog";var e;const t=((e=globalThis.__sveltekit_lskyv2)==null?void 0:e.assets)??l;export{t as a,l as b};
